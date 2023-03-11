b5fdabc7a1bbb2a2eaa20dc45ba02640f4bcadd8
Nao Argouse <nargouse@student.42.fr>
2023-03-11 19:18:22 +0100

    change config.local.php with paheko to garradin to match the namespace in the code

--- config.local.php
+++ config.local.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Paheko;
+namespace Garradin;
 
 // Your custom domain url. MUST end with a '/'. Use it to force HTTPS or custom domain.
 //const WWW_URL = 'https://paheko.chezmoi.tld/';
