20e0277db27afa2f90a3e1877811519ea47c66f7
Reach <reach@42paris.fr>
2023-12-16 18:00:32 +0100

    Add link to the config doc

--- config.local.php
+++ config.local.php
@@ -2,5 +2,7 @@
 
 namespace Paheko;
 
+// Documentation: https://fossil.kd2.org/paheko/wiki?name=Configuration
+
 // Your custom domain url. MUST end with a '/'. Use it to force HTTPS or custom domain.
 //const WWW_URL = 'https://paheko.chezmoi.tld/';
